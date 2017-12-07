37cbc85b960c3d44004a1b360b36c920bf71535a
Michael Adkins <madkinszane@gmail.com>
2017-12-07 02:10:50 -0600

    nd stimulus estimation

--- replay_classification/clusterless.py
+++ replay_classification/clusterless.py
@@ -54,20 +54,19 @@ def poisson_mark_likelihood(marks, joint_mark_intensity_functions=None,
         Instantaneous probability of observing a spike given mark vector
         from data. The parameters for this function should already be set,
         before it is passed to `poisson_mark_likelihood`.
-    ground_process_intensity : array, shape (n_signals, n_states,
-                                             n_place_bins)
+    ground_process_intensity : array, shape (n_signals, 
+                                             n_tuning_bins)
         Probability of observing a spike regardless of marks.
     time_bin_size : float, optional
 
     Returns
     -------
-    poisson_mark_likelihood : array_like, shape (n_signals, n_place_bins)
+    poisson_mark_likelihood : array_like, shape (n_signals, n_tuning_bins)
 
     '''
     probability_no_spike = np.exp(-ground_process_intensity * time_bin_size)
     joint_mark_intensity = np.array(
-        [[jmi(signal_marks) for jmi in jmi_by_state]
-         for signal_marks, jmi_by_state
+         [jmi(signal_marks) for signal_marks, jmi
          in zip(marks, joint_mark_intensity_functions)])
     return joint_mark_intensity * probability_no_spike
 
@@ -104,7 +103,7 @@ def evaluate_mark_space(test_marks, training_marks=None,
 
 def joint_mark_intensity(marks, training_marks=None,
                          mark_std_deviation=None,
-                         place_field=None, place_occupancy=None):
+                         tuning_curve=None, stimulus_occupancy=None):
     '''Evaluate the multivariate density function of the marks and place
     field for each signal
 
@@ -112,10 +111,10 @@ def joint_mark_intensity(marks, training_marks=None,
     ----------
     marks : array_like, shape (n_marks,)
         Marks to be evaulated.
-    place_field : array_like, shape (n_place_bins, n_training_spikes)
-        Response of the signal to position.
-    place_occupancy : array_like, shape (n_place_bins,)
-        How often the animal is at a position
+    tuning_curve : array_like, shape (n_tuning_bins, n_training_spikes)
+        Response of the signal to stimulus.
+    stimulus_occupancy : array_like, shape (n_tuning_bins,)
+        How often the animal is at a stimulus
     training_marks : array_like, shape (n_training_spikes, n_marks)
         The marks for each spike when the animal is moving
     mark_std_deviation : float, optional
@@ -123,7 +122,7 @@ def joint_mark_intensity(marks, training_marks=None,
 
     Returns
     -------
-    joint_mark_intensity : array_like, shape (n_place_bins,)
+    joint_mark_intensity : array_like, shape (n_tuning_bins,)
 
     '''
     if np.any(~np.isnan(marks)):
@@ -131,15 +130,15 @@ def joint_mark_intensity(marks, training_marks=None,
         mark_space = evaluate_mark_space(
             marks, training_marks=training_marks,
             mark_std_deviation=mark_std_deviation)
-        return np.dot(place_field, mark_space) / place_occupancy
+        return np.dot(tuning_curve, mark_space) / stimulus_occupancy
     else:
-        return np.ones_like(place_occupancy)
+        return np.ones_like(stimulus_occupancy)
 
 
-def build_joint_mark_intensity(position, training_marks, place_bin_centers,
-                               place_std_deviation, mark_std_deviation):
+def build_joint_mark_intensity(stimulus, training_marks, tuning_bin_centers,
+                               tuning_std_deviation, mark_std_deviation):
     '''Make a joint mark intensity function with precalculated quauntities
-    (`training_marks`, `place_field`, 'place_occupancy') preset.
+    (`training_marks`, `tuning_curve`, 'stimulus_occupancy') preset.
 
     The new function only needs to be passed marks to be evaluated because
     the other quantities have already been set. See functools.partial for
@@ -147,15 +146,14 @@ def build_joint_mark_intensity(position, training_marks, place_bin_centers,
 
     Parameters
     ----------
-    position : array, shape (n_time,)
-        Position of the animal over time
-    training_marks : array, shape (n_time, n_marks)
+    stimulus : ndarray, shape (n_time, n_stimulus_dims)
+        Stimulus to decode over time
+    training_marks : ndarray, shape (n_time, n_marks)
         The marks over time
-    place_bin_centers : array, shape (n_positions
-        The marks for each spike when the animal is moving
-    place_std_deviation : float
+    tuning_bin_centers : array, shape (n_stimulus_dims, n_stimulus_bins)
+        The centers of bins for stimulus
+    tuning_std_deviation : float
     mark_std_deviation : float
-
 
     Returns
     -------
@@ -163,102 +161,104 @@ def build_joint_mark_intensity(position, training_marks, place_bin_centers,
 
     '''
     is_spike = np.any(~np.isnan(training_marks), axis=1)
-    place_occupancy = estimate_place_occupancy(
-        position, place_bin_centers, place_std_deviation)
-    place_field = estimate_place_field(
-        position, is_spike, place_bin_centers, place_std_deviation)
+    stimulus_occupancy = estimate_stimulus_occupancy(
+        stimulus, tuning_bin_centers, tuning_std_deviation)
+    tuning_curve = estimate_tuning_curve(
+        stimulus, is_spike, tuning_bin_centers, tuning_std_deviation)
 
     return partial(
         joint_mark_intensity,
         training_marks=training_marks[is_spike, :],
         mark_std_deviation=mark_std_deviation,
-        place_occupancy=place_occupancy,
-        place_field=place_field
+        stimulus_occupancy=stimulus_occupancy,
+        tuning_curve=tuning_curve
     )
 
 
-def estimate_place_field(position, is_spike, place_bin_centers,
-                         place_std_deviation=1):
+def estimate_tuning_curve(stimulus, is_spike, tuning_bin_centers,
+                         tuning_std_deviation=1):
     '''Non-parametric estimate of the neuron receptive field with respect
-    to place.
+    to a stimulus
 
-    Puts a Gaussian with a mean at the position the animal is located at
-    when there is a spike
+    Puts a Gaussian with a mean at the stimulus associated with the animal
+    at each spike. The product of the Gaussians along the stimulus dimension
+    yields a multivariate Gaussian with a diagonal covariate matrix
 
     Parameters
     ----------
-    position : array, shape (n_time,)
-        Position of the animal over time
+    stimulus : narray, shape (n_time, n_stimulus_dims)
+        n-dimensional stimulus over time
     is_spike : array, shape (n_time,)
         Boolean array with True indicating spike at that time.
-    place_bin_centers : array_like, shape (n_place_bins,)
+    tuning_bin_centers : array_like, shape (n_tuning_bins, n_stimulus_dims)
         Evaluate the Gaussian at these values
-    place_std_deviation : float, optional
+    tuning_std_deviation : float, optional
         Standard deviation of the Gaussian kernel
 
     Returns
     -------
-    place_field_estimator : array, shape (n_place_bins, n_spikes)
+    tuning_curve_estimator : array, shape (n_tuning_bins, n_spikes)
 
     '''
     return _normal_pdf(
-        place_bin_centers[:, np.newaxis], mean=position[is_spike],
-        std_deviation=place_std_deviation)
+        tuning_bin_centers[:, np.newaxis], mean=stimulus[is_spike],
+        std_deviation=tuning_std_deviation).prod(axis=-1)
 
 
-def estimate_ground_process_intensity(position, marks, place_bin_centers,
-                                      place_std_deviation):
+def estimate_ground_process_intensity(stimulus, marks, tuning_bin_centers,
+                                      tuning_std_deviation):
     '''The probability of observing a spike regardless of mark.
 
     Marginalizes the joint mark intensity over the mark space.
 
     Parameters
     ----------
-    position : array, shape (n_time,)
-        Position of the animal over time
+    stimulus : narray, shape (n_time, n_stimulus_dims)
+        n-dimensional stimulus over time
     marks : array, shape (n_time, n_marks)
-    place_bin_centers : array, shape (n_place_bins,)
-    place_std_deviation : float
+    tuning_bin_centers : array, shape (n_tuning_bins,)
+    tuning_std_deviation : float
 
     Returns
     -------
-    ground_process_intensity : array, shape (n_place_bins,)
+    ground_process_intensity : array, shape (n_tuning_bins,)
 
     '''
     is_spike = np.any(~np.isnan(marks), axis=1)
-    place_field = estimate_place_field(
-        position, is_spike, place_bin_centers, place_std_deviation)
-    place_occupancy = estimate_place_occupancy(
-        position, place_bin_centers, place_std_deviation)
-    return place_field.sum(axis=1) / place_occupancy
+    tuning_curve = estimate_tuning_curve(
+        stimulus, is_spike, tuning_bin_centers, tuning_std_deviation)
+    stimulus_occupancy = estimate_stimulus_occupancy(
+        stimulus, tuning_bin_centers, tuning_std_deviation)
+    return tuning_curve.sum(axis=1) / stimulus_occupancy
 
 
-def estimate_place_occupancy(position, place_bin_centers,
-                             place_std_deviation=1):
-    '''How often the animal is at a position
+def estimate_stimulus_occupancy(stimulus, tuning_bin_centers,
+                             tuning_std_deviation=1):
+    '''How often the animal is at a stimulus
 
     Denominator in equation #12 and #13 of [1]
 
     Parameters
     ----------
-    position : array, shape (n_time,)
-        Position of the animal over time
-    place_bin_centers : array_like, shape (n_place_bins,)
-    place_std_deviation : float, optional
+    stimulus : narray, shape (n_time, n_stimulus_dims)
+        n-dimensional stimulus over time
+    tuning_bin_centers : array_like, shape (n_tuning_bins, n_stimulus_dims)
+    tuning_std_deviation : float, optional
 
     Returns
     -------
-    place_occupancy : array, shape (n_place_bins,)
-        How often the animal is at a position
-
+    stimulus_occupancy : array, shape (n_tuning_bins,)
+        How often the animal is at a stimulus
     '''
-    return _normal_pdf(
-        place_bin_centers[:, np.newaxis], mean=position,
-        std_deviation=place_std_deviation).sum(axis=1)
+    return _normal_pdf(tuning_bin_centers[:, np.newaxis, :], mean=stimulus,
+                std_deviation=tuning_std_deviation).prod(axis=-1).sum(axis=1)
 
+    # A new axis is introduced to find the difference for each stimulus value
+    # The product of the single gaussians yields a multivariate gaussian over all dimensions
+    # The sum yields a result over all stimulus values seen
 
 def estimate_marginalized_joint_mark_intensity(
-    mark_bin_centers, training_marks, place_field, place_occupancy,
+    mark_bin_centers, training_marks, tuning_curve, stimulus_occupancy,
         mark_std_deviation):
     '''
 
@@ -266,13 +266,13 @@ def estimate_marginalized_joint_mark_intensity(
     ----------
     mark_bin_centers : array, shape (n_mark_bins,)
     training_marks : array, shape (n_spikes, n_marks)
-    place_field : array, shape (n_place_bins, n_spikes)
-    place_occupancy : array, shape (n_place_bins,)
+    tuning_curve : array, shape (n_tuning_bins, n_spikes)
+    stimulus_occupancy : array, shape (n_tuning_bins,)
     mark_std_deviation : float
 
     Returns
     -------
-    marginalized_joint_mark_intensity : array, shape (n_place_bins,
+    marginalized_joint_mark_intensity : array, shape (n_tuning_bins,
                                                       n_mark_bins, n_marks)
 
     '''
@@ -280,5 +280,5 @@ def estimate_marginalized_joint_mark_intensity(
     mark_at_spike = _normal_pdf(
         mark_bin_centers[:, np.newaxis, np.newaxis], training_marks,
         mark_std_deviation)
-    return (np.dot(place_field, mark_at_spike) /
-            place_occupancy[:, np.newaxis, np.newaxis])
+    return (np.dot(tuning_curve, mark_at_spike) /
+            stimulus_occupancy[:, np.newaxis, np.newaxis])
