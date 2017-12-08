5a004f4f6aca458f288428bcb7b1243bdcee81fd
Michael Adkins <adkin099@umn.edu>
2017-12-08 13:02:28 -0600

    fixed indentation

--- mst_decoder/utils.py
+++ mst_decoder/utils.py
@@ -1,8 +1,9 @@
 import numpy as np
 
 def atleast_2d(arr):
-	''' A version of numpy.atleast_2d that makes sure that the original 
-	1d shape is the first dimension in 2d '''
+    ''' A version of numpy.atleast_2d that makes sure that the original 
+    1d shape is the first dimension in 2d
+    '''
     a = np.atleast_2d(arr)
     if a.shape[0] != arr.shape[0]:
         a = a.transpose()
